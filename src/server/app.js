3824914a88b0aa75e54b140d23c36c4c420b5a8a
AdamTaz <adamtarrant@hotmail.com>
2018-03-20 15:17:28 +0000

    wip - initial setup of project

--- src/server/app.js
+++ src/server/app.js
@@ -4,6 +4,9 @@ const https = require('https');
 const multer = require('multer');
 const fs = require('fs');
 const path = require('path');
+const passport = require('passport');
+const session = require('express-session');
+const helmet = require('helmet');
 
 //Config
 const config = process.env.NODE_ENV == 'production' ? require('./config/config_prod.js') : require('./config/config_dev.js');
@@ -11,12 +14,19 @@ const config = process.env.NODE_ENV == 'production' ? require('./config/config_p
 //Init of express app
 const app = express();
 
-
 //Init of multer
 const upload = multer({dest: path.join(__dirname, './uploads/')});
 
 //Static middleware
-app.use(express.static(path.join(__dirname, '../../public')));
+app.use(helmet());
+app.use(session({
+    secret: config.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.static(path.join(__dirname, '../../public'))); 
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('There was an error on the server');
@@ -36,5 +46,5 @@ app.all('/*', (req, res) => {
 
 //Exported functions and objects
 module.exports = {
-
+    app
 };
